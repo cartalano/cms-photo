cfdc00fd088e4e8e157fb1f19768640346970d95
Nicolas Catalano <nicolascatalano1@gmail.com>
2025-10-15 14:44:49 +0200

    correction middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,20 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000',              
+        'https://cartalano.github.io',        
+      ],
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,7 @@
-module.exports = ({ env }) => ({
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
-  proxy: true,
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://localhost:1337'), // utile en prod
   app: { keys: env.array('APP_KEYS') },
+  proxy: true,
 })
